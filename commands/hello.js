55a0625170920d867d8bd28553c465337d236cd4
Ryn's <39035843+RynKings@users.noreply.github.com>
2022-07-05 22:58:35 +0800

    Adding another listener like image, video etc

--- commands/hello.js
+++ commands/hello.js
@@ -9,7 +9,34 @@ var hook = {command: 'help'};
 rh.on_hook('mention', hook, async (c, m) => {
     await c.sendMessage(m.parse.to, {text: 'Hello ' + m.pushName});
 })
+
 var hook = {};
+rh.on_hook('sticker', hook, async (c, m) => {
+	await c.sendMessage(m.parse.to, {text: 'Sticker ' + m.pushName});
+})
+
+var hook = {command: '#convert'};
+rh.on_hook('caption', hook, async (c, m) => {
+	await c.sendMessage(m.parse.to, {text: 'Caption ' + m.pushName});
+})
+
+var hook = {command: 'exec'};
 rh.on_hook('mention', hook, async (c, m) => {
-    await c.sendMessage(m.parse.to, {text: 'Hey ' + m.pushName});
+    let sep = m.parse.text.split('\n');
+    let text = m.parse.text.replace(sep[0] + '\n', '');
+    
+    const print = async function(text){
+		await c.sendMessage(m.parse.to, {text: text});
+	}
+    const codeFormat = (text) => "```\n" + text + "\n```";
+
+    await c.sendMessage(m.parse.to, {text: 'Executing\n' + codeFormat(text)});
+	f = `(async () => {
+		try {
+			${text}
+		} catch (e){
+			c.sendMessage(m.parse.to, codeFormat(String(e.stack)))
+		}
+	})()`
+	eval(f)
 })
